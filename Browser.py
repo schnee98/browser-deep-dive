509509f29e25e4c7a0f8f2450590397fd1eb0bd1
schnee98 <jooeun06161@gmail.com>
2025-11-30 19:05:43 +0900

    feat: 리사이즈 반응 추가

--- Browser.py
+++ Browser.py
@@ -9,22 +9,26 @@ class Browser:
   def __init__(self):
     self.window = tkinter.Tk()
     self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
-    self.canvas.pack()
+    self.canvas.pack(fill=tkinter.BOTH, expand=True)
     self.scroll = 0
+    self.width = WIDTH
+    self.height = HEIGHT
     self.window.bind("<Down>", self.scroll_down)
     self.window.bind("<Up>", self.scroll_up)
     self.window.bind("<MouseWheel>", self.wheel)
+    self.window.bind("<Configure>", self.on_resize)
   
   def load(self, url):
     body = URL(url).request()
     text = lex(body)
-    self.display_list = layout(text)
+    self.text = text
+    self.display_list = layout(text, self.width)
     self.draw()
   
   def draw(self):
     self.canvas.delete("all")
     for x, y, c, in self.display_list:
-      if y > self.scroll + HEIGHT: continue
+      if y > self.scroll + self.height: continue
       if y + VSTEP < self.scroll: continue
 
       self.canvas.create_text(x, y - self.scroll, text=c)
@@ -41,8 +45,15 @@ class Browser:
     self.scroll -= VSTEP
     self.draw()
 
+  def on_resize(self, event):
+    self.width = event.width
+    self.height = event.height
+    if hasattr(self, 'text'):
+      self.display_list = layout(self.text, self.width)
+      self.draw()
 
-def layout(text):
+
+def layout(text, width=WIDTH):
   display_list = []
   cursor_x, cursor_y = HSTEP, VSTEP
   for content in text:
@@ -53,7 +64,7 @@ def layout(text):
 
     display_list.append((cursor_x, cursor_y, content))
     cursor_x += HSTEP
-    if cursor_x >= WIDTH - HSTEP:
+    if cursor_x >= width - HSTEP:
       cursor_y += VSTEP
       cursor_x = HSTEP
 
